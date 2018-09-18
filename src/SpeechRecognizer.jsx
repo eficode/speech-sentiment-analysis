ae0edf44a7a68b9b2b868fd3ca6a1ad5275cb6e8
Marko Klemetti <marko.klemetti@gmail.com>
2018-09-18 16:22:20 +0300

    Add sentiment

--- src/SpeechRecognizer.jsx
+++ src/SpeechRecognizer.jsx
@@ -1,12 +1,47 @@
 import React, { Component, Fragment } from 'react';
 import SpeechRecognition from 'react-speech-recognition';
 
+const SENTIMENT_ENDPOINT = 'https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
+const SENTIMENT_APIKEY = process.env.APIKEY || '';
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Ocp-Apim-Subscription-Key': SENTIMENT_APIKEY,
+};
+
 class SpeechRecognizer extends Component {
   constructor(props) {
     super(props);
 
     const { recognition } = props;
     recognition.lang = 'fi-FI';
+
+    this.state = { sentiment: null };
+  }
+
+  getSentiment = async () => {
+    const { transcript } = this.props;
+
+    const data = {
+      documents: [{
+        id: "1",
+        text: transcript,
+      }],
+    };
+
+    const request = {
+      uri: SENTIMENT_ENDPOINT,
+      params: {
+        headers,
+        method: 'POST',
+        body: JSON.stringify(data),
+      },
+    }
+
+    const response = await fetch(request.uri, request.params);
+    const result = await response.json();
+
+    this.setState({ sentiment: result.documents[0].score });
   }
 
   reset = () => {
@@ -17,6 +52,7 @@ class SpeechRecognizer extends Component {
 
   render() {
     const { transcript, recognition, resetTranscript, browserSupportsSpeechRecognition } = this.props;
+    const { sentiment } = this.state;
 
     if (!browserSupportsSpeechRecognition) {
       return null;
@@ -26,6 +62,9 @@ class SpeechRecognizer extends Component {
       <Fragment>
         <h3>Sanelemasi teksti</h3>
         <p style={{ minHeight: '50px' }}>{ transcript }</p>
+        <p>Sentimentti: { sentiment ? parseFloat(sentiment).toFixed(2) : 'Ei tiedossa' }</p>
+
+        <button onClick={this.getSentiment} className="uk-button uk-button-secondary uk-width-1-1">Analysoi</button>
 
         <button onClick={resetTranscript} className="uk-button uk-button-default uk-width-1-1">Tyhjennä</button>
       </Fragment>
